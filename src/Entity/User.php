f8237013ee2c2add35d78b071adb953c09f5c1c2
Antoine <kromgrim21@gmail.com>
2023-01-31 13:42:34 +0100

    Dashboard setup /admin role/ catgorie

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -68,8 +68,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
-
         return array_unique($roles);
     }
 
@@ -127,4 +125,5 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
 }
